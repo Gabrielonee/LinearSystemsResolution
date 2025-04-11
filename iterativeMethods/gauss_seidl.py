2faffb076cdffbb6dc3140101735ba372eb7b592
Gabrielonee <128603552+Gabrielonee@users.noreply.github.com>
2025-04-11 10:22:35 +0200

    Fixed gauss_seidl

--- iterativeMethods/gauss_seidl.py
+++ iterativeMethods/gauss_seidl.py
@@ -2,33 +2,35 @@ import numpy as np
 import time
 from directMethods import triang_inf
 
-def gauss_seidl(A, b, x0, tol,nmax):
+def fixed_gauss_seidl(A, b, x0, tol, nmax):
     M, N = A.shape
     L = x0.shape[0]
     
     if M != N:
-        print('Matrix A is not a square matrix')
+        print('Matrix A has to be square')
         return None, None, None, None
     elif L != M:
-        print('Dimensions of matrix A does not match dimension of initial guess x0')
+        print('shape(A) != shape(x0)')
         return None, None, None, None
     
     if np.any(np.diag(A) == 0):
-        print('At least a diagonal entry is zero. The method automatically fails')
+        print('At least a diagonal entry is zero')
         return None, None, None, None
     
-    L = np.tril(A) #lower triangular
-    B = A-L
+    L_matrix = np.tril(A) #Lower triangular
+    B = A - L_matrix
     xold = x0.copy()
-    xnew = xold + 1 
+    xnew = x0.copy() 
     nit = 0
 
     start_time = time.time()
-    while np.lianlg.norm(xnew-xold, np.inf) > tol and nit < nmax:
+    while nit < nmax:
         xold = xnew.copy()
-        xnew = triang_inf(L, (b-B@ xold))
+        xnew = triang_inf(L_matrix, b - B @ xold)
+        if np.linalg.norm(xnew - xold, np.inf) < tol:
+            break
         nit += 1
 
-    elapsed_time = start_time - time.time()
-    err = np.linalg.norm(xnew - xold, np.inf)
+    elapsed_time = time.time() - start_time
+    err = np.linalg.norm(b - A @ xnew) / np.linalg.norm(xnew)
     return xnew, nit, elapsed_time, err
