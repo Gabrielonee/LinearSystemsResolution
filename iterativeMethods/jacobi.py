d2a88e6371ad74aa8498d5be5d189b695d39b0f6
Francesco <romeo.francesco.2002@gmail.com>
2025-04-18 18:21:26 +0200

    jacobi updated and new utilities

--- iterativeMethods/jacobi.py
+++ iterativeMethods/jacobi.py
@@ -1,49 +1,46 @@
+import scipy.sparse as sp
 import numpy as np
-import time
 
-def jacobi(A, b, x0, tol, nmax): 
-    M, N = A.shape
-    L = x0.shape[0]
-    
-    if M != N:
-        print('Matrix A is not a square matrix')
-        return None, None, None, None
-    elif L != M:
-        print('Dimensions of matrix A does not match dimension of initial guess x0')
-        return None, None, None, None
-    
-    if np.any(np.diag(A) == 0):
-        print('At least a diagonal entry is zero. The method automatically fails')
-        return None, None, None, None
-    
-    D = np.diag(np.diag(A)) #diagonal
-    B = D - A
-    
-    xold = x0.copy()
-    xnew = xold + 1 
-    nit = 0 #iteration number
-    
-    start_time = time.time()
-    
-    while np.linalg.norm(xnew - xold, np.inf) > tol and nit < nmax: #if over thresold or nmax
-        xold = xnew.copy()
-        xnew = np.linalg.inv(D) @ (B @ xold + b) #iterate the computing of solution
+
+def jacobi_solver(A, b, tol=1e-6, x0=None, nmax=20000):
+    """
+        Metodo jacobi
+    """
+    if x0 is None:
+        x = np.zeros_like(b)
+    else:
+        x = x0.copy()
+
+    if (not sp.issparse(A)):
+        A_sparse = sp.csr_matrix(A)
+    else:
+        A_sparse = A
+
+    D = A_sparse.diagonal()
+    R = A_sparse - sp.diags(D)
+
+    nit = 0
+    x_new = np.ones(b.size)
+
+    for _ in range(nmax):
+        x_new = (b - R @ x) / D
+        if np.linalg.norm(x_new - x, np.inf) < tol:
+            break
+        x = x_new
         nit += 1
-    
-    elapsed_time = time.time() - start_time
-    err = np.linalg.norm(xnew - xold, np.inf)
-    
-    return xnew, nit, elapsed_time, err
+
+    err = np.linalg.norm(x_new - x, np.inf)
+
+    return x_new, nit, err
+
 
 A = np.array([[4, -1, 0], [-1, 4, -1], [0, -1, 4]])
 b = np.array([1, 2, 3])
 x0 = np.zeros_like(b)
-tol = 1e-6
-nmax = 10000
 
-x, nit, time_elapsed, err = jacobi(A, b, x0, tol, nmax)
+x, nit, err = jacobi_solver(A, b, tol=1e-6)
 
 print(f"Solution: {x}")
 print(f"Iterations: {nit}")
-print(f"Time elapsed: {time_elapsed:.6f} seconds")
 print(f"Final error: {err:.2e}")
+
---
+++ utilities/readMatrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.io
+import scipy.sparse as sp
+
+
+def MatrixReader(filePath=None):
+    if filePath is None:
+        raise ValueError("Path file not provided! Use a valid path")
+
+    A = scipy.io.mmread(filePath)  # Lettura matrice sparse
+    return A
+
+
+# Funzione per verificare l'accuratezza
+def verify_accuracy(x_computed, x_true):
+    error = np.linalg.norm(x_computed - x_true, ord=np.inf)  # Calcola l'errore
+    return error
