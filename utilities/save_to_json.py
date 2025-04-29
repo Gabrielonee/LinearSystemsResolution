6d42e486eee51f0621c45424d8eb679fb1a763fa
Gabrielonee <128603552+Gabrielonee@users.noreply.github.com>
2025-04-29 18:39:01 +0200

    Little fix

--- utilities/save_to_json.py
+++ utilities/save_to_json.py
@@ -3,20 +3,6 @@ from pathlib import Path
 
 
 def save_results_to_json(results, matrix_name, output_dir="results_json"):
-    """
-    Salva un array di SolverResult in un file JSON, utile per analisi future.
-
-    Parametri
-    ----------
-    results : list of SolverResult
-        Lista dei risultati da salvare.
-
-    matrix_name : str
-        Nome della matrice, usato per il nome del file JSON.
-
-    output_dir : str, default="results_json"
-        Cartella dove salvare i file JSON.
-    """
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # Convertiamo tutti i SolverResult in dizionari
