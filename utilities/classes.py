6f833b5e74a1164ffc45dd217df4d23c6a99c7c8
Francesco <romeo.francesco.2002@gmail.com>
2025-05-30 10:32:13 +0200

    maybe the last dance

--- utilities/classes.py
+++ utilities/classes.py
@@ -6,6 +6,7 @@ import numpy as np
 class IterativeResult:
     solution: np.ndarray
     iterations: int
+    converged: bool
 
 
 @dataclass
@@ -19,23 +20,35 @@ class SolverResult:
     memory_kb: float
 
     def __str__(self):
-        return (
-            f"Method: {self.method_name}\n"
-            f"Tolerance: {self.tol:.1e}, Max Iterations: {
-                self.max_iterations}\n"
-            f"Iterations Performed: {self.method_result.iterations}\n"
-            f"Relative Error: {self.rel_error:.2e}\n"
-            f"Time: {self.time_seconds:.4f} s, Memory: {
-                self.memory_kb:.2f} KB\n"
-        )
+        if not self.method_result.converged:
+            return (
+                f"Method: {self.method_name}\n"
+                f"Tolerance: {self.tol:.1e}, Max Iterations: {self.max_iterations}\n"
+                f"Iterations Performed: {self.method_result.iterations}\n"
+                f"Convergence Failed, Relative Error: {self.rel_error:.2e}\n"
+                f"Time: {self.time_seconds:.4f} s, Memory: {self.memory_kb:.2f} KB\n"
+            )
+        else:
+            return (
+                f"Method: {self.method_name}\n"
+                f"Tolerance: {self.tol:.1e}, Max Iterations: {
+                    self.max_iterations}\n"
+                f"Iterations Performed: {self.method_result.iterations}\n"
+                f"Relative Error: {self.rel_error:.2e}\n"
+                f"Time: {self.time_seconds:.4f} s, Memory: {
+                    self.memory_kb:.2f} KB\n"
+            )
 
     def to_dict(self):
-        return {
-            "method": self.method_name,
-            "tolerance": self.tol,
-            "max_iterations": self.max_iterations,
-            "performed_iterations": self.method_result.iterations,
-            "rel_error": float(self.rel_error),
-            "time_seconds": self.time_seconds,
-            "memory_kb": self.memory_kb,
-        }
+        if self.method_result.converged:
+            return {
+                "method": self.method_name,
+                "tolerance": self.tol,
+                "max_iterations": self.max_iterations,
+                "performed_iterations": self.method_result.iterations,
+                "rel_error": float(self.rel_error),
+                "time_seconds": self.time_seconds,
+                "memory_kb": self.memory_kb,
+            }
+        else:
+            return {}
